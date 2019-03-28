0203d51e5079648fdf4a77f3f338178c4d4de774
hgwak <hannelgwak@gmail.com>
2019-03-27 19:30:01 -0700

    checking to see if state will affect a display none element'

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -14,45 +14,62 @@ import FilterCrimes from './crimes/filtercrimes';
 import Error404 from './general/error_404';
 import AppHeader from './general/header';
 
-const App = () => {
-    document.onscroll=()=>{
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            document.getElementById("scrollButton").style.display = "block";
-        } else {
-            document.getElementById("scrollButton").style.display = "none";
+class App extends Component {
+
+    state = {
+        isShowing: false
+    }
+
+    componentDidMount() {
+        document.onscroll=()=>{
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("scrollButton").style.display = "block";
+                this.setState({
+                    isShowing: true
+                })
+            } else {
+                document.getElementById("scrollButton").style.display = "none";
+                this.setState({
+                    isShowing: false
+                })
+            }
         }
     }
-    if(window.location.pathname==='/stats'){
-        return (
-            <Fragment>
-                <Route path="/stats" component={Chart}/>
-                <Link to="/" className="btn-floating btn waves-effect waves-light blue backButton">
-                    <i className="large material-icons center-align">arrow_back</i>
-                </Link>
-            </Fragment>
-        )
+    
+    render() {
+        if(window.location.pathname==='/stats'){
+            return (
+                <Fragment>
+                    <Route path="/stats" component={Chart}/>
+                    <Link to="/" className="btn-floating btn waves-effect waves-light blue backButton">
+                        <i className="large material-icons center-align">arrow_back</i>
+                    </Link>
+                </Fragment>
+            )
+        }
+        else{
+            return(
+                <div className="outer-div">
+                    <AppHeader/>
+                    <Switch>
+                        <Route exact path="/" component={HomePage}/>
+                        <Route exact path="/violent" component={HomePage}/>
+                        <Route exact path="/property" component={HomePage}/>
+                        <Route path="/area/:area_code" component={AreaId}/>
+                        <Route path="/crimes/:crime_code" component={CrimeId}/>
+                        <Route path="/filtered-crimes/:area/:crime_code" component={FilterCrimes}/>
+                        <Route path="/dr/:dr_number" component={DrId}/>
+    
+                        <Route component={Error404}/>
+                    </Switch>
+                    <a onClick={()=>{document.body.scrollTop = 0; document.documentElement.scrollTop = 0;}} id="scrollButton" className={`btn-floating btn waves-effect waves-light ${this.state.isShowing ? 'showing' : ''} `}>
+                        <i className="large material-icons center-align centerArrow">arrow_upward</i>
+                    </a>
+                </div>
+            )
+        }
     }
-    else{
-        return(
-            <div className="outer-div">
-                <AppHeader/>
-                <Switch>
-                    <Route exact path="/" component={HomePage}/>
-                    <Route exact path="/violent" component={HomePage}/>
-                    <Route exact path="/property" component={HomePage}/>
-                    <Route path="/area/:area_code" component={AreaId}/>
-                    <Route path="/crimes/:crime_code" component={CrimeId}/>
-                    <Route path="/filtered-crimes/:area/:crime_code" component={FilterCrimes}/>
-                    <Route path="/dr/:dr_number" component={DrId}/>
-
-                    <Route component={Error404}/>
-                </Switch>
-                <a onClick={()=>{document.body.scrollTop = 0; document.documentElement.scrollTop = 0;}} id="scrollButton" className="btn-floating btn waves-effect waves-light">
-                    <i className="large material-icons center-align centerArrow">arrow_upward</i>
-                </a>
-            </div>
-        )
-    }
+    
 };
 
 export default App;
